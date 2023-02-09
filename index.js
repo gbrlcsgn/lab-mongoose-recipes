d50e0432ac519a1bc8a35cdb931aa690d6b0bc6a
gbrlcsgn <gabrielgiacomin@yahoo.com.br>
2023-02-09 16:19:57 +0000

    final

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://127.0.0.1:27017/recipe-app";
 
 //Method 1 : Using Async Await
 
@@ -19,9 +19,31 @@ const manageRecipes = async () => {
     await Recipe.deleteMany();
 
     // Run your code here, after you have insured that the connection was made
+    await Recipe.create({
+      title: "Hot Dog Fingers",
+      level: "UltraPro Chef",
+      ingredients: ["5 hot dogs", "5 human fingers", "100g of cut off nails"],
+      cuisine: "Anthropofagic",
+      dishType: "snack",
+      duration: 120,
+      creator: "Chef Hannibal",
+    });
+    console.log("Hot Dog Fingers");
   } catch (error) {
     console.log(error);
   }
+
+  await Recipe.insertMany(data);
+  data.forEach((Recipe) => console.log(Recipe.title));
+
+  await Recipe.findOneAndUpdate(
+    { title: "Rigatoni alla Genovese" },
+    { duration: 100 }
+  );
+  console.log("Great Success!");
+
+  await Recipe.deleteOne({ title: "Carrot Cake" });
+  console.log("Very Gut!");
 };
 
 manageRecipes();
